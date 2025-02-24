a1b8b3d798d35085cb6db63e32345dc9d0b6220f
Manivel Nagarajan <write2manivel@gmail.com>
2025-02-24 18:27:40 +0530

    Create EssentialFeed macOS framework

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,14 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Manivel Nagarajan on 24/02/25.
+//
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
+
